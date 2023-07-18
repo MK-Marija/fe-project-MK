70ff2fb7ebf1d28af11502b78a2ef80c712c8cd8
Marija Kocevska <kocevska_m@hotmail.com>
2023-07-18 16:30:31 +0100

    task 4

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,21 +1,43 @@
 import React, { useEffect,useState,useContext } from "react"
+import {useParams} from "react-router-dom"
 import axios from 'axios'
+import { getArticles } from "../utils/api"
 
 const Articles = () => {
+   
     const[articles, setArticles] = useState([])
-}
+
 
 useEffect(() => {
-    axios.get("")
-    .then(({res})=> {
-       return res.json();
-    })
-    .then((data) => {
-        setArticles(data.articles)
-    })
-})
+   getArticles().then((articles) => {
+   setArticles(articles)
+   })
+},[])
+
 
 
     return (
-        <div className="body">
-                    </div> )
+       <div>
+        <h1>Articles</h1>
+       {articles.map((article) => {
+        return (
+            <div key={article.article_id}> 
+            <h2>
+             {article.title}
+            <p>{article.topic}</p>
+            <p>{article.author}</p>
+            <p> {article.body}</p>
+            <p>{article.created_at}</p>
+            <p> {article.votes}</p>
+             <img src={article.article_img_url} alt="Article Image" />
+             
+
+            </h2> </div>
+        )
+       }           
+        ) }
+        </div> 
+        )
+    }
+
+    export default Articles
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Home = () => {
+return (
+
+        <div className="body">
+            <div> Welcome to NC News <br></br> News is the first draft of history</div>
+        </div>
+
+)
+}
+
+export default Home
